22b74b3369da9739e98945d1f8db93bd3d9a0633
cdk0507 <cdk0507@naver.com>
2015-08-19 20:36:36 +0900

    idChk() 메소드 완료

--- src/kr/co/syncbook/web/MemberController.java
+++ src/kr/co/syncbook/web/MemberController.java
@@ -1,10 +1,16 @@
 package kr.co.syncbook.web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.syncbook.biz.MemberService;
@@ -23,7 +29,7 @@ public class MemberController {
 	public String joinForm(){
 		return "joinForm";
 	}
-	@RequestMapping("login")
+	@RequestMapping("/login")
 	public ModelAndView login(String id, String password, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		boolean flag = memberService.memberLogin(id, password);
@@ -31,21 +37,35 @@ public class MemberController {
 			session.setAttribute("id", id);
 			mav.setViewName("index");
 		}
-		else mav.setViewName("member/login");
+		else mav.setViewName("loginForm");
 		return mav;
 	}
-	@RequestMapping("logout")
+	@RequestMapping("/logout")
 	public String logout(HttpSession session){
 		session.removeAttribute("id");
 		return "index";
 	}
 	
-	@RequestMapping("join")
-	public ModelAndView join(MemberVO member) {
+	@RequestMapping("/join")
+	public ModelAndView join(MemberVO member, @RequestParam String post1, @RequestParam String post2) {
 		ModelAndView mav = new ModelAndView();
-		boolean flag = memberService.memberJoin(member);
-		if(flag) mav.setViewName("member/login");
-		else mav.setViewName("member/join");
+		boolean flag = memberService.memberJoin(member, post1, post2);
+		if(flag) {
+			mav.setViewName("loginForm");
+		}
+		else {
+			mav.setViewName("joinForm");
+		}
 		return mav;
+	}
+	@RequestMapping("/idCheck")
+	public void idCheck(HttpServletRequest request, HttpServletResponse response, @RequestParam String memberId) throws IOException {
+		boolean flag = memberService.idCheck(memberId);
+		String result = null;
+		PrintWriter out = response.getWriter();
+		if(flag) result = "fail";
+		else result = "success";
+		out.print(result);
+		out.close();
 	}
 }
